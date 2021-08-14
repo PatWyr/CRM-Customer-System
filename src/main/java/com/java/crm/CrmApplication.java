9ae28bb82dc550a576d12f55fec52e14dbb91adf
Patrick <74612989+PatWyr@users.noreply.github.com>
2021-08-14 20:44:51 +0200

    Added css file

--- src/main/java/com/java/crm/CrmApplication.java
+++ src/main/java/com/java/crm/CrmApplication.java
@@ -3,10 +3,10 @@ package com.java.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 @EnableJpaRepositories("com.java.crm.repository")
-@SpringBootApplication
+@SpringBootApplication()
 public class CrmApplication {
 
 
